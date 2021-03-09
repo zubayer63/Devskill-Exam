72492c8121f235a64f0f2e57d239a6907ff0d360
zubayer alam <zubayer63cse@gmail.com>
2021-03-09 22:31:40 +0600

    Problem 6

---
+++ AdmissionTest/Problem6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem6
+{
+    class Program
+    {
+        
+        public void MyWork(Func<int, int> logic)
+        {
+
+        }
+
+        
+        public void MyLogic(int t)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
