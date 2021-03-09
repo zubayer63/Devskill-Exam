7b622b3e5f2e1656cc5ac96ad6098fcbf0739eb9
zubayer alam <zubayer63cse@gmail.com>
2021-03-09 22:19:54 +0600

    Problem 5

---
+++ AdmissionTest/Problem5/Button.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem5
+{
+    public delegate void Del(string str);
+    class Button
+    {
+        public event Del OnClick;
+
+        public void Click()
+        {
+       
+            OnClick("hello");
+        }
+
+        protected virtual void OnClicked(string str)
+        {
+            OnClick?.Invoke(str);
+        }
+
+    }
+
+   
+}
