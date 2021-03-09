e910dfd3c4b4788ffc388445acc1219e2f3ffb11
zubayer alam <zubayer63cse@gmail.com>
2021-03-09 21:38:41 +0600

    Added problem 1

---
+++ AdmissionTest/Problem1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>
+           {
+               new Student(){name= "Rahim", age=29},
+               new Student(){name= "Karim", age=30},
+               new Student(){name= "Ashiq", age=23},
+               new Student(){name= "Masud", age=30},
+               new Student(){name= "Ashiq", age=30},
+
+
+
+           };
+            var sortList = student.OrderBy(s => s.name).ThenByDescending(s => s.age);
+            foreach (var item in sortList)
+            {
+                Console.WriteLine(item.name + " " + item.age);
+            }
+        }
+    }
+
+    class Student
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+        public int age
+        {
+            get;
+            set;
+        }
+    }
+}
