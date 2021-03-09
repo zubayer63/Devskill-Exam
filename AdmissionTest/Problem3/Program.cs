e89bfc9fac610ffad38a913e34f288dd2c1669e4
zubayer alam <zubayer63cse@gmail.com>
2021-03-09 21:51:26 +0600

    Problem3

---
+++ AdmissionTest/Problem3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public class SuperTree<T> where T : class,new()
+    {
+
+    }
+}
